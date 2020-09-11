2fd5a76cb148a39280a03fd54f691bb557296422
Shaurabh Saha <shaurabh.saha@tatacommunications.com>
2020-09-11 11:20:26 +0530

    Created form and edited layout

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
 # __name__ is equivalent to __main__ in normal python
+
+# To increses sec and prevent cookie changes
+app.config['SECRET_KEY'] = 'be3412f8c73cfe0ee86bc5fbef1094c4'
 
 #dummy data
 posts=[
@@ -30,6 +35,20 @@ def home():
 def about():
     # return '<h1>About Page </h1>'
     return render_template('about.html',title='About')
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+    	flash(f'Account created for {form.username.data}!','success')
+    	return redirect(url_for('home'))
+    return render_template('register.html',title='Register',form=form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html',title='Login',form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
